8bd54415f9baaa96301d7205d7cbe8b72e9de539
Swapnali98 <61203937+Swapnali98@users.noreply.github.com>
2020-11-08 19:13:28 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,85 +1,181 @@
-const apikey="9044f55e4b9e4ad39054d37f77e6457a";
-var article_area=document.getElementById("news-articles");
 
-//function to have formatted news in form of json//
 
-function getNews(news)
+document.onreadystatechange = function() { 
+  if (document.readyState !== "complete") { 
+      document.querySelector( 
+        "body").style.visibility = "hidden"; 
+      document.querySelector( 
+        "#loader").style.visibility = "visible"; 
+  } else { 
+      document.querySelector( 
+        "#loader").style.display = "none"; 
+      document.querySelector( 
+        "body").style.visibility = "visible"; 
+  } 
+};
+debugger;
+
+  getdata();
+
+  function search(){
+
+    
+      var searchData = document.getElementById("search").value;
+      const key = "897c2e65625e425396495470aef7fadd";
+
+      const url = `https://newsapi.org/v2/everything?q=${searchData}&apiKey=${key}`
+
+UserSearch();
+async function UserSearch(){
+  const response  = await fetch(url);
+  const dataOf = await response.json();
+  console.log(dataOf);
+  if(dataOf.articles.length == 0)
+  {
+
+  
+    var nodata = ` <div class="not-found">No article was found based on the search.</div>`;
+    document.getElementById("news-articles").innerHTML = nodata;
+    console.log("else executed");
+  
+    
+} // end of then               
+else
 {
-    let output="";
-    let totalresult="";
-    console.log(news)
-    console.log(news.totalResults)
-    if(news.totalResults>0){
-        news.articles.forEach(element => {
-            output+=
-            `
-            <section class= "container">
-            <li class ="article"><a class="article-link" href="${element.url}" target= " _blank">
-            <div class="img_area">
-            <img src ="${element.urlToImage}" class="article-img" alt="${element.title}"></img>
-            </div>
-            <h2 class="article_title">${element.title}
-            </h2>
-            
-            </a>
-            <li class="article-description">${element.description || "description not available"}</li>
-            <div class ="article-author">-${element.author ? element.author:"Anon"}</div><br>
-            
-            </li>
-            </section>
-            
-            `
-            ;
-        });
-            article_area.innerHTML=output;
-        }
-        else
-        {
-               article_area.innerHTML='<p class="not-found" no article was found based on serach><p>';
+  dataOf.articles.forEach(article =>{
 
-        }
-            
+    const title = article.title;
+    const img = article.urlToImage;
+    const description = article.description;
+    const auth = article.author;
+    const link = article.url;
+    const source = article.source.name;
+ //paste below
+ /*------------------------------------------------------------ */
+ squery+=`
+ <div class="main_card" style="width:400px;">
+  
+ <li class="article">
+   
+     <article>
        
-    };
+        <img class="article-img" src="${img}" alt="no image"  width="100%"/>
+         
+        <h2 class="article-title" style="font-weight:bold;">${title}-${source}</h2> 
+         
+         <p class="article-description">${description}</p>
+         
+        <span class="article-author">${auth}</span><br><br><br> 
+         
+         <a class="article-link" href="${link}"><b>Full article</b></a><br><br><br><br>
+ 
+    </article>
 
-
-    //async function wth await
-async function loading(searchValueWord=""){
-
-    article_area.innerHTML='<li class="load">News are loading</li>'
-
-    if(searchValueWord!="")
-    {
-        url=`http://newsapi.org/v2/everything?q=${searchValueWord}=At&apikey=${apikey}`;
-    }
-    else{
-        url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}`;
-    }
-    //api call//
-    const response=await fetch(url);
-    const result=await response.json();
-    getNews(result);
+     </li>
+ </div>
+ 
+ `;
+ document.getElementById("news-articles").innerHTML = squery;
+/*------------------------------------------------------------------- */
+  })   //paste above
+  var squery =+ ''
+  
+  
 }
 
-//text value is get from searchbar and pass to retrive function//
-async function searchValue(event)
-{
-    if(event.which === 13||event.keycode===13||event.key === "Enter")
-    {
-        console.log(event);
-        console.log(event.which);
-        console.log(event.key);
-        console.log(event.keycode);
 
-        loading(event.target.value);
-        console.log(event.target.value);
-    }
+
+} //end of for loop
+
+// searchData.addEventListener("input", function(event){
+
+//   if(!this.value){
+//     document.getElementById("news-articles").innerHTML = out;
+//   }
+  
+//   })
+
+
+  }
+
+ 
+
+//Toggling the dark mode
+  function myFunction() {
+    var element = document.body;
+    
+    element.classList.toggle("dark-mode");
+   
+    
+ }
+
+ //fetching the data
+
+async function getdata() {
+  const res = await fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=897c2e65625e425396495470aef7fadd');
+  const data = await res.json();
+  
+  console.log(data);
+  
+ 
+  data.articles.forEach(news =>{
+    
+    const title = news.title;
+    const img = news.urlToImage;
+    const description = news.description;
+    const auth = news.author;
+    const link = news.url;
+    const source = news.source.name;
+
+
+
+    out+=`
+   <div class="main_card" style="width:400px;">
+     
+    <li class="article">
+      
+         
+       <article>
+       
+           <img class="article-img" src="${img}" alt="no image"  width="100%">
+            
+            <h2 class="article-title" style="font-weight:bold;">${title}-${source}</h2>
+            
+            <p class="article-description">${description}</p>
+            
+            <span class="article-author">-${auth}</span><br><br><br>
+            
+            <a class="article-link" href="${link}">Full article</a><br><br><br><br>
+    
+        </article>
+        
+       
+     </li>
+    
+     </div>
+  
+    
+    
+    `;
+    document.getElementById("news-articles").innerHTML = out;
+        
+  })
+ var out =+ '';
+
+
+
+
 }
 
-//attached event listener //
+ 
 
-function start(){
-console.log("start function called in onload")
-document.getElementById("search").addEventListener("keypress",searchValue);
-loading();
-}
+
+//search results apear on hitting enter
+
+let input = document.getElementById("search");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   search();
+  }
+});
